4bad91ee4a96f9a685336b703b481cca3f74966e
zztinChina <158575696+zztinChina@users.noreply.github.com>
2024-02-26 22:23:49 +0800

    Add files via upload

--- 2.21/FileName.cpp
+++ 2.21/FileName.cpp
@@ -1,6 +1,12 @@
 #include<stdio.h>
-void my_strcpy(char* arr1,char* arr2)
+void my_strcat(char* arr1, const char* arr2)
 {
+	//int count = 0;
+
+	while (*arr1)
+	{
+		arr1++;
+	}
 	while (*arr1++ = *arr2++)
 	{
 		;
@@ -9,8 +15,8 @@ void my_strcpy(char* arr1,char* arr2)
 int main()
 {
 	char arr[100] = "asdcddds";
-	char arr2[] = "asdddsdddddddd";
-	 my_strcpy(arr, arr2);
+	char arr2[] = "asdddsddddheheh";
+	 my_strcat(arr, arr2);
 	printf("%s", arr);
 	return 0;
 }
